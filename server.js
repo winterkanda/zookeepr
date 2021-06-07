16a400a53396ba561fd267ac0bfd2ab2d9a2a2e5
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-07 15:37:29 -0700

    added valdiation data function

--- server.js
+++ server.js
@@ -76,8 +76,12 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
+    // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
   // add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals);
+    const animal = createNewAnimal(req.body, animals);
 
   res.json(req.body);
 });
@@ -85,3 +89,19 @@ app.post('/api/animals', (req, res) => {
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+}
