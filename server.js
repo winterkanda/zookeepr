0c0be1a31174d426e781c22e62f2d1f3ed156f6e
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-07 17:13:03 -0700

    added html route to server.js

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
