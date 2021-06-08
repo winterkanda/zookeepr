576483b950edb24f6dd886812e052449b88d8c41
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-07 18:31:18 -0700

    added index.html file to server.js

--- server.js
+++ server.js
@@ -93,6 +93,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+  
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
