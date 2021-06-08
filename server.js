84129167c11a9ab74d05c858daa7780939dfe3e3
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-07 21:18:32 -0700

    added zookeeper.html route to server.js

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -95,6 +96,10 @@ app.get('/', (req, res) => {
 
 app.get('/animals', (req, res) => {
     res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
   
 app.listen(PORT, () => {
