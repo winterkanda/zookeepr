3d252912eed81f0a727a5be6f740c93f10d92207
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-07 17:53:10 -0700

    added css and js to server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
